06696ae0b1913c5a116cc1bd1d340373d7105a1e
Shawntribuce1234 <shawntribuce@gmail.com>
2025-05-30 21:10:42 -0400

    Add files via upload

--- scroll_effects.js
+++ scroll_effects.js
@@ -1,31 +1,31 @@
 <script>
     const videos = document.querySelectorAll(".scroll-video");
-    videos.forEach(video => {
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    if (video.paused && video.currentTime === 0) {
-                        video.play();
-                    }
+    videos.forEach(video =>{
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                if (video.paused && video.currentTime === 0) {
+                    video.play();
                 }
-            });
-        }, {
-            threshold: 0.9
+            }
         });
-        observer.observe(video);
+    }, {
+        thheshold: 0.5
     });
+    observer.observe(video);
+});
 
     const wipeImage = document.querySelector(".wipe-up");
-    if (wipeImage) {
-        const imageObserver = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    wipeImage.classList.add("animate");
-                }
-            });
-        }, {
-            threshold: 0.9
+
+    const imageObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                wipeImage.classList.add("animate");
+            }
         });
-        imageObserver.observe(wipeImage);
-    }
+    }, {
+        threshold: 0.9
+    });
+    imageObserver.observe(wipeImage);
+
 </script>
