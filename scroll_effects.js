de0240de12e4b7b2ad9f85f2cb0e98e5f6df7725
Shawntribuce1234 <shawntribuce@gmail.com>
2025-05-30 20:21:16 -0400

    Updated Javascript

--- scroll_effects.js
+++ scroll_effects.js
@@ -1,31 +1,31 @@
 <script>
     const videos = document.querySelectorAll(".scroll-video");
-    videos.forEach(video =>{
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                if (video.paused && video.currentTime === 0) {
-                    video.play();
+    videos.forEach(video => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    if (video.paused && video.currentTime === 0) {
+                        video.play();
+                    }
                 }
-            }
+            });
+        }, {
+            threshold: 0.5
         });
-    }, {
-        threshold: 0.5
+        observer.observe(video);
     });
-    observer.observe(video);
-});
 
     const wipeImage = document.querySelector(".wipe-up");
-
-    const imageObserver = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                wipeImage.classList.add("animate");
-            }
+    if (wipeImage) {
+        const imageObserver = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    wipeImage.classList.add("animate");
+                }
+            });
+        }, {
+            threshold: 0.9
         });
-    }, {
-        threshold: 0.9
-    });
-    imageObserver.observe(wipeImage);
-
+        imageObserver.observe(wipeImage);
+    }
 </script>
